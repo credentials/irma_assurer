000b7f80f97a8fd1a6936ef64d054ee0db9e40be
Anthirian <anthirian@users.noreply.github.com>
2014-11-20 16:11:36 +0100

    Initial version of Passport Decoder

--- src/org/irmacard/identity/common/PassportDataDecoder.java
+++ src/org/irmacard/identity/common/PassportDataDecoder.java
@@ -8,8 +8,22 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 import java.util.List;
 
 public class PassportDataDecoder extends ByteToMessageDecoder {
+    byte[] incoming;
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        // Determine buffer size and allocate resources
+        int size = in.readInt();
+        incoming = new byte[size];
 
+        // Store data
+        try {
+            in.readBytes(incoming);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+
+        // Send acknowledgement
+        out.add(CONSTANTS.ACK);
     }
 }
