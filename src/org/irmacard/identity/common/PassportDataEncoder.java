95b9317035d5b114388e932a93d772eeb00d578f
Anthirian <anthirian@users.noreply.github.com>
2014-12-22 14:34:36 +0100

    Convert Passport object to bytes using streams

--- src/org/irmacard/identity/common/PassportDataEncoder.java
+++ src/org/irmacard/identity/common/PassportDataEncoder.java
@@ -2,15 +2,16 @@ package org.irmacard.identity.common;
 
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufOutputStream;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import net.sourceforge.scuba.util.Hex;
 import org.jmrtd.Passport;
 import org.jmrtd.lds.LDS;
-import org.jmrtd.lds.LDSFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class PassportDataEncoder extends MessageToByteEncoder {
     @Override
@@ -18,43 +19,26 @@ public class PassportDataEncoder extends MessageToByteEncoder {
         if (msg instanceof Passport) {
             Passport passport = (Passport) msg;
             LDS source = passport.getLDS();
-            int numberOfBytes = 0;
-
-            List<LDSFile> fileList = new ArrayList<LDSFile>();
 
             for (Short fileID : source.getFileList()) {
                 try {
-                    fileList.add(source.getFile(fileID));
+                    System.out.printf("Creating streams.\n");
+                    InputStream input = source.getInputStream(fileID);
+                    OutputStream output = new ByteBufOutputStream(out);
+                    String entryName = Hex.shortToHexString(fileID) + ".bin";
+                    System.out.printf("Converting %s.\n", entryName);
+                    int bytesRead = 0;
+                    byte[] dgBytes = new byte[1024];
+                    System.out.printf("Starting read from datagroup %s.\n", fileID.byteValue());
+                    while((bytesRead = input.read(dgBytes)) > 0){
+                        System.out.printf("Read %d bytes from the input stream.\n", bytesRead);
+                        output.write(dgBytes);
+                        System.out.printf("Bytes written to output stream.\n");
+                    }
                 } catch (IOException ioe) {
-                    System.err.printf("Skipping DataGroup %s. %s\n", fileID.byteValue(), ioe.getMessage());
+                    System.err.printf("Skipping DataGroup %s. %s.\n", fileID.byteValue(), ioe.getMessage());
                 }
-
             }
-
-            for (LDSFile f : fileList) {
-                numberOfBytes += f.getLength();
-            }
-
-            // dest should hold the entire LDS, plus an integer decribing size, plus a one-byte magic number 'P'
-            byte[] dest = new byte[numberOfBytes + 19]; // FIXME: Not sure why 19 extra bytes are needed
-            int writeOffset = 0;
-
-            try {
-                for (LDSFile f : fileList) {
-                    // tag (int) + value (byte[]) = encoded
-                    byte[] bytes = f.getEncoded();
-                    int size = bytes.length;
-                    System.arraycopy(bytes, 0, dest, writeOffset, size);
-                    writeOffset += size;
-                }
-            } catch (IndexOutOfBoundsException e) {
-                System.err.printf("Out of bounds. There was too much data to fit in the buffer.\n");
-            }
-
-            // Write the encoded passport to the output stream
-            out.writeByte('P');
-            out.writeInt(numberOfBytes);
-            out.writeBytes(dest);
         } else {
             System.err.printf("This message is not a passport. Skipping.\n");
         }
